d5816c4eb12927ce8dbf0c0ad589b448e8df3826
Daniil Hirchyts <daniil.hirchyts@etu.umontpellier.fr>
2022-09-26 07:58:18 +0200

    TD 1 TD 2

--- TD2/script-somme-la-plus-proche.php
+++ TD2/script-somme-la-plus-proche.php
@@ -7,9 +7,9 @@
     else {
         $s1 = $s - $nb;
         $s2 = $nb - ($s - $i);
-        if ($s1 < $s2) echo $s;
-        else echo $s - $i;
+        if ($s1 < $s2) $f = $s;
+        else $f = $s - $i;
     }
-
+    echo "###$f###";
 
 
