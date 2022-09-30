79274f2e5304524cabdf66abacd6e09fb9cd9917
Daniil Hirchyts <daniil.hirchyts@etu.umontpellier.fr>
2022-09-30 11:18:35 +0200

    TD 1 TD 2 TD 3

--- TD1/script-somme-0.php
+++ TD1/script-somme-0.php
@@ -3,10 +3,8 @@ $s = 0;
 $i = 0;
 $n = readline();
 while ($n != 0) {
-    if ($n > 0) {
-        $s += $n;
-        ++$i;
-    }
+    $s += $n;
+    ++$i;
     $n = readline();
 }
 echo ($i);
--- TD2/script-nombre-de-valeurs-paires.php
+++ TD2/script-nombre-de-valeurs-paires.php
@@ -1,10 +1,8 @@
 <?php
+$v = readline("Entrez une valeur: ");
+$n = 0;
+while ($v != 0) {
+    if ($v % 2 == 0) ++$n;
     $v = readline("Entrez une valeur: ");
-    $n = 0;
-    while ($v != 0) {
-        if ($v % 2 == 0) {
-            ++$n;
-        }
-        $v = readline("Entrez une valeur: ");
-    }
-    echo ($n);
+}
+echo (++$n);
