5e9848af816240f680c68181f65302b94e3fb148
Daniil Hirchyts <daniil.hirchyts@etu.umontpellier.fr>
2022-10-07 10:12:18 +0200

    TD 3 - Complete

--- TD3/fonction-aChiffresTousDifferents.php
+++ TD3/fonction-aChiffresTousDifferents.php
@@ -1,9 +1,9 @@
 <?php
 
 require "fonction-fréquenceChiffres.php";
-function aChiffresTousDifferents(int $n): string
+function aChiffresTousDifferents(int $n): bool
 {
     $tab = fréquenceChiffres($n);
-    for($i=0;$i<10;$i++) if($tab[$i]>1) return "faux";
-    return "vrai";
+    for($i=0;$i<10;$i++) if($tab[$i]>1) return false;
+    return true;
 }
