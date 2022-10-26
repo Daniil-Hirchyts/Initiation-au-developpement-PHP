4099db3a632ea7c7ac9014b35cc0d0fb916ff76b
Daniil Hirchyts <daniil.hirchyts@etu.umontpellier.fr>
2022-10-26 09:37:39 +0200

    .

--- TD2/script-longueur-dernier-mot.php
+++ TD2/script-longueur-dernier-mot.php
@@ -1,14 +1,10 @@
 <?php
 $n = 0;
-$m = 0;
 $c = "";
 while ($c != ".") {
     $c = fgetc(STDIN);
     if ($c != ' ') ++$n;
-    if ($c == ' ') {
-        $m = $n;
-        $n = 0;
-    }
+    else $n = 0;
+    if ($c == '.') $n--;
 }
-if ($n >= $m) $m = $n - 1;
-echo "***$m***";
+echo "***$n***";
