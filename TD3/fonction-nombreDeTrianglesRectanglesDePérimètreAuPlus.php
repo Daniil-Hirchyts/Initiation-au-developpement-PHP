bf99e032368c23dd84ea4e82a00974a71b0bda9e
Daniil Hirchyts <daniil.hirchyts@etu.umontpellier.fr>
2022-11-04 18:57:31 +0100

    .

---
+++ TD3/fonction-nombreDeTrianglesRectanglesDePérimètreAuPlus.php
@@ -0,0 +1,22 @@
+<?php
+
+// fichier fonction-nombreDeTriangleRectangleDePérimètreAuPlus.php
+
+/**
+ * Donnée : un entier
+ * Résultat : retourne le nombre de triangles rectangles dont le périmètre est au plus n
+ */
+function nombreDeTriangleRectangleDePérimètreAuPlus(int $n): int
+{
+    $nb = 0;
+    for ($i = 1; $i <= $n; $i++) {
+        for ($j = $i; $j <= $n; $j++) {
+            for ($k = 1; $k <= $n; $k++) {
+                if ($i + $j + $k <= $n && $i * $i + $j * $j == $k * $k) {
+                    $nb++;
+                }
+            }
+        }
+    }
+    return $nb;
+}
